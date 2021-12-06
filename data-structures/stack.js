8bb836878bcbcaabeae7f76d4bc08c511869b188
Cristina Lester <cristina.deelester@gmail.com>
2021-12-06 14:25:56 -0800

    added stack implementation

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,26 +1,42 @@
-/**
- * Using an array as a stack to implement a function that
- * determines if a word is a palindrome or not
- */
+function Stack() {
+  //keeps track of the number of elements in the stack
+  this.count = 0;
+  //keeps track of the data
+  this.storage = {};
 
-function isPalindrome(word) {
-  //this is my stack as an array
-  let letters = [];
-  let reversedWord = "";
+  /**
+   * Add the element at the end of the stack
+   * @param {*} el
+   */
+  this.push = (el) => {
+    this.storage[this.count] = el;
+    this.count++;
+  };
 
-  //push adds at the end every letter
-  for (let i = 0; i < word.length; i++) {
-    letters.push(word[i]);
-  }
+  /**
+   * Removes and returns the element at the end of the stack
+   */
+  this.pop = () => {
+    if (this.count === 0) return undefined;
+    let item = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return item;
+  };
 
-  //pop removes from the end and returns the removed item
-  for (let i = 0; i < word.length; i++) {
-    reversedWord += letters.pop();
-  }
-)
-  //if the reversed word is the same than the initial word, then it was a palindrome
-  return (word===reversedWord);
+  /**
+   * Returns the size of the stack
+   * @returns the size of the stack
+   */
+  this.size = () => {
+    return this.count;
+  };
 
+  /**
+   * Returns the last element in the stack
+   * @returns the element at the end of the stack
+   */
+  this.peek = () => {
+    return this.storage[this.count - 1];
+  };
 }
-
-isPalindrome("racecar");
