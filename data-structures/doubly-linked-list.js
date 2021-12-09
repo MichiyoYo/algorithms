44cb4d50e3389000323b389d90f8d0be07b9e9e4
Cristina Lester <cristina.deelester@gmail.com>
2021-12-08 17:24:46 -0800

    implemente push for DDL

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -8,8 +8,32 @@ class Node {
 
 class DoublyLinkedList {
   constructor() {
-    this.lenght = 0;
+    this.length = 0;
     this.head = null;
     this.tail = null;
+  }
+
+  /**
+   * Adds a new node at the end of the list
+   * @param {*} val the value of the new node
+   * @returns the updated list
+   */
+  push(val) {
+    //make new node
+    let newNode = new Node(val);
+    //assign head and/or tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //assign pointers
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    //increase length
+    this.length++;
+    //return
+    return this;
   }
 }
