a1e3129f1fc45e385c4e64dbaae3de992fba2150
Cristina Lester <cristina.deelester@gmail.com>
2021-12-27 19:12:24 -0800

    added addEdge method to graph class

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -3,8 +3,16 @@ class Graph {
     this.adjacencyList = {};
   }
 
-  addVertex(val) {
-    this.adjacencyList[val] = [];
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    return this.adjacencyList;
+  }
+
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+    return this.adjacencyList;
   }
 }
 
