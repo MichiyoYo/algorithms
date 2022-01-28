3a74f30f13a6fb8d25656688b5f99d35d67abc26
Cristina Lester <cristina.deelester@gmail.com>
2022-01-28 11:53:14 -0800

    added set method to has tables

--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -5,14 +5,34 @@ class HashTable {
     this.keyMap = new Array(size);
   }
 
-  _hash(key, arrayLen) {
+  _hash(key) {
     let total = 0;
     let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       let char = key[i];
       let value = char.charCodeAt(0) - 96;
-      total = (total * WEIRD_PRIME + value) % arrayLen;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
     return total;
   }
+
+  set(key, value) {
+    //hash the key
+    let index = this._hash(key);
+    //store the key-value pair in the hash taable array via separate chaining
+    if (this.keyMap[index]) {
+      this.keyMap[index].push([key, value]);
+    } else {
+      this.keyMap[index] = [[key, value]];
+    }
+    return index;
+  }
+
+  get(key) {
+    //hashes the key
+    //retrieve the value associated with key
+    //if not in the hash table return undefined
+  }
 }
+
+let hashTab = new HashTable(100);
