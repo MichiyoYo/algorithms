8c69dfa4e1e1833a6ec53d4427a602caac8bc164
Cristina Lester <cristina.deelester@gmail.com>
2022-01-28 11:08:56 -0800

    added hash table class

--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -1,33 +1,18 @@
-/**
- * The problem with this function is that it only hashes strings
- * Not constant time, but linear in key length
- * Could be a little more random
- */
-
-function hashV1(key, arrayLen) {
-  let total = 0;
-
-  for (let char of key) {
-    let value = char.charCodeAt(0) - 96;
-    //using % will always give me a key in the range [0, arrayLen]
-    total = (total + value) % arrayLen;
+class HashTable {
+  //we want the size of the array to be a prime number
+  constructor(size = 53) {
+    //the array were we will store the key-value pairs
+    this.keyMap = new Array(size);
   }
 
-  return total;
-}
-
-/**
- * In this version we multiply the total by a prime number
- * to minimize the number of collisions;
- * We also set a max for looping, the minimum between 100 and the length of the key
- */
-function hashV2(key, arrayLen) {
-  let total = 0;
-  let WEIRD_PRIME = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * WEIRD_PRIME + value) % arrayLen;
+  _hash(key, arrayLen) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
   }
-  return total;
 }
