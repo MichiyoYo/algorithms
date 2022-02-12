9839576859f5e678889905d10a313a519acbd8d8
Cristina Lester <cristina.deelester@gmail.com>
2022-02-11 16:23:36 -0800

    added challenge

--- challenges/findLargestSmallestTree.js
+++ challenges/findLargestSmallestTree.js
@@ -1,4 +1,16 @@
+/**
+ * Given a root of a Binary Search Tree (BST) and a number num, implement an efficient function findLargestSmallerKey that finds the largest key in the tree that is smaller than num.
+ * If such a number doesn't exist, return -1. Assume that all keys in the tree are nonnegative.
+ */
 function findLargestSmallestTree(root, num) {
   if (!root) return -1;
-  let parent = root;
+  if (!root.left && !root.right && root.val < num) return root.val;
+  if (num < root.val) {
+    let k = findLargestSmallestTree(root.left, val);
+    if (k === -1) return root.val;
+    else return k;
+  }
+  if (num > root.val) {
+    return findLargestSmallestTree(root.right, val);
+  }
 }
